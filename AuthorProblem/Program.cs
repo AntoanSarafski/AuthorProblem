3faa0f2dde6fee7164955bbdc5bf23cb5bd4b8f1
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-22 14:11:08 +0200

    Commit with solution for problem which Author written current method.

--- AuthorProblem/Program.cs
+++ AuthorProblem/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AuthorProblem
 {
@@ -8,6 +11,24 @@ namespace AuthorProblem
     {
         [Author("Vanio")]
         public static void Main(string[] args)
+        {
+            Type type = typeof(StartUp);
+
+            foreach (var method in type.GetMethods((BindingFlags) 60))
+            {
+                
+                AuthorAttribute[] attributes = method.GetCustomAttributes<AuthorAttribute>().ToArray();
+
+                foreach (var attr in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                }
+
+            }
+        }
+
+        [Author("Pepi")]
+        public static void OtherMethod()
         {
 
         }
