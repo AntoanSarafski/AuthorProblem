32efece8192cacaa1e6e0e5e77cd278d33fba4d1
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-22 14:00:57 +0200

    Add project files.

---
+++ AuthorProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Antoan")]
+    [Author("Antoana")]
+    public class StartUp
+    {
+        [Author("Vanio")]
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
